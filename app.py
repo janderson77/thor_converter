e10c084e5a6432b1dd400028a124cd7132cbe06d
janderson77 <jamesseventyseven@gmail.com>
2021-09-12 18:08:02 -0600

    Implemented file selection and masterfile conversion. Does not serve files yet

--- app.py
+++ app.py
@@ -1,21 +1,43 @@
-from flask import Flask
+import os
+from flask import Flask, request, flash, redirect
 from flask.templating import render_template
 from flask_debugtoolbar import DebugToolbarExtension
 from forms import FileForm
+import json
+from masterfile_api import convert_masterfile
+
 
 app = Flask(__name__)
 
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 toolbar = DebugToolbarExtension(app)
 
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
 ALLOWED_EXTENSIONS = {'xls', 'xlsx'}
 
 
 def allowed_file(filename):
     return '.' in filename and \
-            filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSIONS
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/')
+
+@app.route('/', methods=["GET", "POST"])
 def show_home_page():
+    c = open('clients.json')
+    data = json.load(c)
+    d = []
+    for i in data['Clients']:
+        d.append(i['name'])
     form = FileForm()
+    form.client.choices = d
+
+    if form.validate_on_submit():
+        f = request.files.getlist(form.convertFile.name)
+        for i in f:
+            if 'masterfile' in i.filename:
+                convert_masterfile(i)
+        print(f)
+        return redirect('/')
     return render_template("home.html", form=form)
--- forms.py
+++ forms.py
@@ -1,8 +1,11 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed, FileRequired
-from wtforms import StringField, SelectField
+from flask_wtf.file import FileAllowed, FileRequired
+from wtforms import SelectField, MultipleFileField
 from wtforms.validators import DataRequired
 
+
 class FileForm(FlaskForm):
-    convertFile = FileField('Excel file (.xls, .xlsx)',
-        validators=[FileRequired(),FileAllowed(['xls', 'xlsx'])],render_kw={'accept': '.xls, .xlsx'})
+    client = SelectField('Which client?', render_kw={
+                         'class': "form-select form-control"}, description="Select a client")
+    convertFile = MultipleFileField('One or More Excel Files (.xls, .xlsx)',
+                                    validators=[DataRequired(), FileAllowed(['xls', 'xlsx'])], render_kw={'accept': '.xls, .xlsx', 'class': 'form-control'})
