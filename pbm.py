cdca53fdbc772a0770c2bcaa596370eca2075009
janderson77 <jamesseventyseven@gmail.com>
2022-10-20 14:08:31 -0600

    Documented some code

--- pbm.py
+++ pbm.py
@@ -19,23 +19,34 @@ def convertPBM(export):
     # Iterates over the sheet
     for row in xlsSheet:
         temp_data = []
+        # Filters out rows with unusable data
         if 'Grand Total:' in row or "Page" in row or "Page " in row:
             continue
+        # Filters out a specific row with unusable data the previous filter couldn't catch
         if len(row[0]) > 12:
             continue
+        # Finds the row where the employee ID lives
         if "Regular Hours:" in row:
+            # Extracts the employee ID from the number in this cell
             tempID = int(row[0][4:])
             continue
+        # Skips over rows with dates in them by looking for a "/"
         if "/" in row[0]:
             continue
+        # All other rows will either be blank, or have the employee's hours data.
+        # A blank row will be iterated over, but no action taken.
+        # A row with numerical data must be the employee's hours data, which is extracted.
         for index, value in enumerate(row):
             if row[index] != "":
                 if type(value) == int:
                     temp_data.append(float(value))
                 else:
                     temp_data.append(round(value,2))
+        # If temp_data's length is greater than 0, that means all employee data has been found
         if len(temp_data) > 0:
+            # Creates a new instance of the Employee dataclass, and inserts data into it
             tc = newTimecard(tempID, temp_data[3], temp_data[4])
+            # Adds this Employee to the data list
             data.append(tc)
 
     return create_pbm_import(data)
