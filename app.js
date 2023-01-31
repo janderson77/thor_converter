14f7036babfffcbbbee5d52a1489bc894ad08913
janderson77 <jamesseventyseven@gmail.com>
2023-01-31 14:50:38 -0700

    Added error handling to the frontend. Added random phrases to be loaded to the button

--- app.js
+++ app.js
@@ -9,16 +9,56 @@ const client_list = {
 const phrases = ["Crack the Sky", "Let Your Hammer Fly", "Call Down the Lightning",
 "To Valhalla and Back", "Release Asgaard's Fury"]
 
+let phraseIndex = Math.floor(Math.random() * phrases.length)
+
 $(() => {
+    // Initial load function.
+    // Checks if the backend is up and running.
+    // Displays an error if it is not.
     $.ajax({
         url: 'http://127.0.0.1:5000/',
         type: 'GET',
         cache: false,
         contentType: false,
         processData: false,
-    }).done((data, textStatus, xhr) => {
-        $('#overlay').fadeOut()
+        tryCount : 0,
+        retryLimit : 3,
+        success : () => {
+            console.log(phraseIndex)
+            $('#submit-button').text(phrases[phraseIndex]) 
+            $('#overlay').fadeOut()
+        },
+        error: (xhr, textStatus, errorThrown) => {
+            if(textStatus == 'timeout') {
+                this.tryCount++;
+                if(this.tryCount <= this.retryLimit) {
+                    $.ajax(this);
+                    return;
+                }
+                $('#overlay').fadeOut()
+                $('#submit-button').prop("disabled", true)
+                $('#submit-button').text("Error: Disabled") 
+                let errorHTML = `<p class="text-white-50 mx-auto mt-2 mb-2">Error: Please reload the page and try again.</p>`
+                $('#errors').append(errorHTML)
+                return;
+            }
+            if(xhr.status == 500) {
+                $('#overlay').fadeOut()
+                $('#submit-button').prop("disabled", true)
+                $('#submit-button').text("Error: Disabled") 
+                let errorHTML = `<p class="text-white-50 mx-auto mt-2 mb-2">Error: Please reload the page and try again.</p>`
+                $('#errors').append(errorHTML)
+            }else{
+                $('#overlay').fadeOut()
+                $('#submit-button').prop("disabled", true)
+                $('#submit-button').text("Error: Disabled") 
+                let errorHTML = `<p class="text-white-50 mx-auto mt-2 mb-2">Error: Please reload the page and try again.</p>`
+                $('#errors').append(errorHTML)
+            }
+        }
     })
+
+    // Sets the select to have the current supported clients/vms'
     $.each(client_list, function(key, value) {   
         $('#client_select')
             .append($("<option></option>")
@@ -26,6 +66,7 @@ $(() => {
                        .text(value)); 
    });
 
+    //Form submission handler
     $('form').on('submit', (e) => {
         e.preventDefault();
 
