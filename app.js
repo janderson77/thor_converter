aecf7d9f72e9ff6d5e5f3688373078364084a3fe
janderson77 <jamesseventyseven@gmail.com>
2023-02-03 21:19:20 -0700

    Changed throgress logic to remove the elements since they were added, not changed from hidden to shown

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const client_list = {
-        "Papa Pita Bakery": "Papa Pita Bakery",
-        "PBM": "PBM",
-        "Novatime": "Novatime",
-        "Nutraceutical": "Nutraceutical",
-        "Maximus": "Maximus"
+    "Papa Pita Bakery": "Papa Pita Bakery",
+    "PBM": "PBM",
+    "Novatime": "Novatime",
+    "Nutraceutical": "Nutraceutical",
+    "Maximus": "Maximus"
 }
 
 const phrases = ["Crack the Sky", "Let Your Hammer Fly", "Call Down the Lightning",
-"To Valhalla and Back", "Release Asgaard's Fury"]
+    "To Valhalla and Back", "Release Asgaard's Fury"]
 
 let phraseIndex = Math.floor(Math.random() * phrases.length)
 
@@ -21,36 +21,36 @@ $(() => {
         cache: false,
         contentType: false,
         processData: false,
-        tryCount : 0,
-        retryLimit : 3,
-        success : () => {
-            $('#submit-button').text(phrases[phraseIndex]) 
+        tryCount: 0,
+        retryLimit: 3,
+        success: () => {
+            $('#submit-button').text(phrases[phraseIndex])
             $('#overlay').fadeOut()
         },
         error: (xhr, textStatus, errorThrown) => {
-            if(textStatus == 'timeout') {
+            if (textStatus == 'timeout') {
                 this.tryCount++;
-                if(this.tryCount <= this.retryLimit) {
+                if (this.tryCount <= this.retryLimit) {
                     $.ajax(this);
                     return;
                 }
                 $('#overlay').fadeOut()
                 $('#submit-button').prop("disabled", true)
-                $('#submit-button').text("Error: Disabled") 
+                $('#submit-button').text("Error: Disabled")
                 let errorHTML = `<p class="text-white-50 mx-auto mt-2 mb-2">Error: Please reload the page and try again.</p>`
                 $('#errors').append(errorHTML)
                 return;
             }
-            if(xhr.status == 500) {
+            if (xhr.status == 500) {
                 $('#overlay').fadeOut()
                 $('#submit-button').prop("disabled", true)
-                $('#submit-button').text("Error: Disabled") 
+                $('#submit-button').text("Error: Disabled")
                 let errorHTML = `<p class="text-white-50 mx-auto mt-2 mb-2">Error: Please reload the page and try again.</p>`
                 $('#errors').append(errorHTML)
-            }else{
+            } else {
                 $('#overlay').fadeOut()
                 $('#submit-button').prop("disabled", true)
-                $('#submit-button').text("Error: Disabled") 
+                $('#submit-button').text("Error: Disabled")
                 let errorHTML = `<p class="text-white-50 mx-auto mt-2 mb-2">Error: Please reload the page and try again.</p>`
                 $('#errors').append(errorHTML)
             }
@@ -58,12 +58,12 @@ $(() => {
     })
 
     // Sets the select to have the current supported clients/vms'
-    $.each(client_list, function(key, value) {   
+    $.each(client_list, function (key, value) {
         $('#client_select')
             .append($("<option></option>")
-                       .attr("value", key)
-                       .text(value)); 
-   });
+                .attr("value", key)
+                .text(value));
+    });
 
     //Form submission handler
     $('form').on('submit', (e) => {
@@ -81,7 +81,7 @@ $(() => {
         inThrogress.setAttribute("class", "text-white-50 mx-auto mt-2 mb-3")
         inThrogress.textContent = "In Throgress..."
 
-        
+
         throgressContainer.append(throgImg)
         throgressContainer.append(inThrogress)
         console.log(throgressContainer)
@@ -93,16 +93,16 @@ $(() => {
         for (let f of upload.files) {
             formData.append("file", f)
         }
-        formData.append("client", client.options[client.selectedIndex].text)        
+        formData.append("client", client.options[client.selectedIndex].text)
 
         let req = new XMLHttpRequest();
-        if (upload.files.length > 1){
+        if (upload.files.length > 1) {
             req.open('POST', url);
             req.responseType = 'blob';
             req.contentType = "application/zip"
             req.onload = (e) => {
                 let contentDispo = e.currentTarget.getResponseHeader('Content-Disposition');
-                let blob = new Blob([e.currentTarget.response], {type: 'application/zip'})
+                let blob = new Blob([e.currentTarget.response], { type: 'application/zip' })
                 let fileName = contentDispo.match(/filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/)[1];
                 let a = document.createElement('a')
                 let url = window.URL.createObjectURL(blob)
@@ -112,28 +112,30 @@ $(() => {
                 a.click()
                 a.remove()
                 window.URL.revokeObjectURL(url)
-                throgressContainer.setAttribute("hidden", "true")
+                throgImg.remove()
+                inThrogress.remove()
             }
             req.send(formData)
-        }else{
+        } else {
             req.open('POST', url);
             req.responseType = 'blob';
             req.contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
             req.onload = (e) => {
                 let contentDispo = e.currentTarget.getResponseHeader('Content-Disposition');
-                let blob = new Blob([e.currentTarget.response], {type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'})
-                let fileName = contentDispo.slice(22, contentDispo.length-1)
+                let blob = new Blob([e.currentTarget.response], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' })
+                let fileName = contentDispo.slice(22, contentDispo.length - 1)
                 let a = document.createElement('a')
                 let url = window.URL.createObjectURL(blob)
-                
+
                 a.setAttribute('download', fileName.trim())
                 a.setAttribute("href", url)
                 document.body.append(a)
                 a.click()
                 a.remove()
                 window.URL.revokeObjectURL(url)
-                throgressContainer.setAttribute("hidden", "true")
-                
+                throgImg.remove()
+                inThrogress.remove()
+
             }
             req.send(formData)
         }
